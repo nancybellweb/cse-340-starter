6b63330573c09b5ffb8515f40fd62a9b5f2ab647
nanurodriguez <nancy95rod@gmail.com>
2026-01-17 11:40:43 -0700

    addding all the queries necessary from the task 1 to the assignment2 file

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -38,5 +38,50 @@ CREATE TABLE IF NOT EXISTS public.account
     CONSTRAINT account_pkey PRIMARY KEY (account_id)
 );
 
+--QUERY 1 : insterting new value into account table
+
+INSERT INTO account (
+	account_firstname,
+	account_lastname,
+	account_email,
+	account_password
+)
+VALUES (
+	'Tony',
+	'Stark',
+	'tony@starkent.com',
+	'Iam1ronM@n'
+);
 
 
+--QUERY 2 : Modyfing TOny Stark account to admin account
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+
+--QUERY 3 : Deleting Tony Stark account from account table
+
+DELETE FROM account
+WHERE account_email = 'tony@starkent.com';
+
+--QUERY 4: updating GM Hummer description using REPLACE()
+
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM'
+AND inv_model = 'Hummer';
+
+--QUERY 5: Using INNER JOIN -- selecting "Sport" inventory items (only items in the "Sport" classification)
+
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory i
+INNER JOIN classification c
+ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport';
+
+--QUERY 6: Updating image paths to include the /vehicles/ directory
+
+UPDATE inventory
+SET
+	inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
