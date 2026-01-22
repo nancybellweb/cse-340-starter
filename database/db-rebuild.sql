9858c5bf59a78f6af5e2b90821c7e30142b0c2fb
nanurodriguez <nancy95rod@gmail.com>
2026-01-22 12:16:39 -0700

    adding a debugger tool

--- database/db-rebuild.sql
+++ database/db-rebuild.sql
@@ -4,7 +4,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO demo123;
+    OWNER TO demo234;
 
 --creating classification table
 
