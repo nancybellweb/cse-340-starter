92d2a504de8e2d167f2cd92f0b8dba73436e4b22
nanurodriguez <nancy95rod@gmail.com>
2026-01-24 13:52:45 -0700

    adding try and catch methods to seee if I can get to find where the error is

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,7 +3,13 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
     const nav = await utilities.getNav()
-    res.render("index", {title: "Home", nav})
-} 
+    if (nav) {
+        res.render("index", { title: "Home", nav })
+    } else {
+            //handle the case where nav could not be built or defined
+            //for example, I can now render an error page or display a fallback value 
+            res.render("index", {title: "Home", nav: []}) //empty nav as fallback
+    }
+}
 
 module.exports = baseController 
