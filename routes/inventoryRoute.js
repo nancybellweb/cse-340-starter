c1646b3d96b870a1a026c88ac55a16a563f132ec
nanurodriguez <nancy95rod@gmail.com>
2026-01-28 10:41:32 -0700

    adding error handling to the footer for testing and also  adding the hadleErrors to the router.get

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,6 +4,6 @@ const router = new express.Router()
 const invController = require("../controllers/invController")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
 module.exports = router;
