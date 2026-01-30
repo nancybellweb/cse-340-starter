a4b3d09d80c95239ee212ae366297b85ecb1d799
nanurodriguez <nancy95rod@gmail.com>
2026-01-30 00:57:17 -0700

    donnne with account  setup

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,6 @@
 const utilities = require("../utilities/")
 
+
 /* ****************************************
 * Deliver login view
 * *************************************** */
@@ -22,8 +23,27 @@ async function buildRegister(req, res, next) {
         errors: null, 
     })// if I need to pass errors, I can do it here
 }
+/* ****************************************
+* Process Registration
+* *************************************** */
+async function registerAccount(req, res) {
+    let nav = await utilities.getNav()
+    const { account_firstname } = req.body
 
-module.exports = { buildLogin, buildRegister }
+    // (database code to register the user would go here)
+    // (database code to add the user to the database would go here)
+
+    req.flash(
+        "notice",
+        `Congratulations, you're registered ${account_firstname}. Please log in.`
+    )
+    res.status(201).render("account/login", {
+        title: "Login",
+        nav,
+    })
+}
+
+module.exports = { buildLogin, buildRegister, registerAccount }
 
 
 
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -4,11 +4,17 @@ const router = new express.Router()
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
 
-// Route to build inventory by classification view
+// login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to deliver registration view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
+// Process the registration 
+router.post(
+    "/register",
+    utilities.handleErrors(accountController.registerAccount)
+)
 
 module.exports = router
 
-module.exports = router;
+console.log(accountController)
+//test
