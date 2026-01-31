e5c4ddec026f70ff3d638d41096da8a2d2d88b49
nanurodriguez <nancy95rod@gmail.com>
2026-01-31 15:22:39 -0700

    updating accountController

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -31,8 +31,22 @@ async function buildRegister(req, res, next) {
 * *************************************** */
 async function registerAccount(req, res) {
     const { account_firstname, account_lastname, account_email, account_password } = req.body
+    let nav = await utilities.getNav()
 
-    const hashedPassword = await bcrypt.hash(account_password, 10)
+    // Hash the password before storing it to database - salt is generated automatically
+    let hashedPassword
+    try {
+        // regular password and cost of 10
+        hashedPassword = await bcrypt.hashSync(account_password, 10)
+    } catch (error) {
+        req.flash("notice", "Sorry, there was an error processing the registration.")
+        res.status(500).render("account/register", {
+        title: "Registration",
+        nav,
+        errors: null,
+        })
+        return
+    }
 
     const regResult = await accountModel.registerAccount(
         account_firstname,
@@ -42,11 +56,17 @@ async function registerAccount(req, res) {
     )
 
     if (regResult) {
+        req.flash("notice", "Congratulations, you are registered. Please log in.")
         res.redirect("/account/login")
     } else {
-        res.render("account/register", { title: "Register", errors: null })
+        res.status(500).render("account/register", {
+        title: "Registration",
+        nav,
+        errors: null,
+        })
     }
-}
+    }
+
 
 
 module.exports = { buildLogin, buildRegister, registerAccount }
