ee0aa9e634d7b7e85cd9290883a86becf3a9afd7
nanurodriguez <nancy95rod@gmail.com>
2026-01-31 12:28:58 -0700

    adding account-model

---
+++ models/account-model.js
@@ -0,0 +1,35 @@
+const pool = require("../database/") 
+
+/* **********************
+ *   Register new account
+ * ********************* */
+async function registerAccount(account_firstname, account_lastname, account_email, account_password){
+    try {
+        const sql = `
+        INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *`
+        const result = await pool.query(sql, [account_firstname, account_lastname, account_email, account_password])
+        return result.rows[0]
+    } catch (error) {
+        return error.message
+    }
+}
+
+/* **********************
+ *   Check for existing email
+ * ********************* */
+async function checkExistingEmail(account_email){
+    try {
+        const sql = "SELECT * FROM account WHERE account_email = $1"
+        const email = await pool.query(sql, [account_email])
+        return email.rowCount
+    } catch (error) {
+        return error.message
+    }
+}
+
+module.exports = {
+    registerAccount,
+    checkExistingEmail
+}
