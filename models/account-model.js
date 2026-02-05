b77776629c9e1651bda43e338bc159e230e15cf0
nanurodriguez <nancy95rod@gmail.com>
2026-02-05 02:21:43 -0700

    updating get account by email function

--- models/account-model.js
+++ models/account-model.js
@@ -33,12 +33,12 @@ async function checkExistingEmail(account_email){
  ***************************** */
 async function getAccountByEmail(account_email) {
     try {
-        const sql = "SELECT * FROM account WHERE account_email = $1"
-        const data = await pool.query(sql, [account_email])
-        return data.rows[0]
+        const result = await pool.query(
+        'SELECT account_id, account_firstname, account_lastname, account_email, account_type, account_password FROM account WHERE account_email = $1',
+        [account_email])
+        return result.rows[0]
     } catch (error) {
-        console.error("getAccountByEmail error:", error)
-        throw error
+        return new Error("No matching email found")
     }
 }
 module.exports = {
