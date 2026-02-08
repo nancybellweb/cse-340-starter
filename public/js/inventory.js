f78559973593c72c510a2def7a13b868b8d8d5fc
nanurodriguez <nancy95rod@gmail.com>
2026-02-08 13:56:07 -0700

    updating the buildInventory function to match the instructions

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -1,62 +1,44 @@
-// Wait for the DOM to load
-
-document.addEventListener("DOMContentLoaded", () => {
-    const classificationList = document.getElementById("classificationList")
-    console.log("classificationList element:", classificationList)
-
-    const inventoryDisplay = document.getElementById("inventoryDisplay")
-
-    classificationList.addEventListener("change", async () => {
-        const classificationId = classificationList.value
-        console.log("Selected ID:", classificationId)
-        // Clear display if nothing selected
-        if (!classificationId) {
-        inventoryDisplay.innerHTML = ""
-        return
-        }
-
-        try {
-        // Fetch JSON data from server
-        const response = await fetch(`/inv/getInventory/${classificationId}`)
-        const data = await response.json()
-
-        // If no vehicles found
-        if (!data.length) {
-            inventoryDisplay.innerHTML = "<p>No inventory found for this classification.</p>"
-            return
-        }
-
-        // Build table
-        let table = `
-            <table>
-            <thead>
-                <tr>
-                <th>Make</th>
-                <th>Model</th>
-                </tr>
-            </thead>
-            <tbody>
-        `
-
-        data.forEach(item => {
-            table += `
-            <tr>
-                <td>${item.inv_make}</td>
-                <td>${item.inv_model}</td>
-            </tr>
-            `
-        })
-
-        table += `
-            </tbody>
-            </table>
-        `
-
-        inventoryDisplay.innerHTML = table
-
-        } catch (error) {
-        console.error("AJAX error:", error)
-        inventoryDisplay.innerHTML = "<p>Error loading inventory.</p>"
-        }
+'use strict' 
+    
+    // Get a list of items in inventory based on the classification_id 
+    let classificationList = document.querySelector("#classificationList")
+    classificationList.addEventListener("change", function () { 
+    let classification_id = classificationList.value 
+    console.log(`classification_id is: ${classification_id}`) 
+    let classIdURL = "/inv/getInventory/"+classification_id 
+    fetch(classIdURL) 
+    .then(function (response) { 
+    if (response.ok) { 
+        return response.json(); 
+    } 
+    throw Error("Network response was not OK"); 
+    }) 
+    .then(function (data) { 
+    console.log(data); 
+    buildInventoryList(data); 
+    }) 
+    .catch(function (error) { 
+    console.log('There was a problem: ', error.message) 
+    }) 
     })
-})
+
+    // Build inventory items into HTML table components and inject into DOM 
+function buildInventoryList(data) { 
+    let inventoryDisplay = document.getElementById("inventoryDisplay"); 
+    // Set up the table labels 
+    let dataTable = '<thead>'; 
+    dataTable += '<tr><th>Vehicle Name</th><td>&nbsp;</td><td>&nbsp;</td></tr>'; 
+    dataTable += '</thead>'; 
+    // Set up the table body 
+    dataTable += '<tbody>'; 
+    // Iterate over all vehicles in the array and put each in a row 
+    data.forEach(function (element) { 
+    console.log(element.inv_id + ", " + element.inv_model); 
+    dataTable += `<tr><td>${element.inv_make} ${element.inv_model}</td>`; 
+    dataTable += `<td><a href='/inv/edit/${element.inv_id}' title='Click to update'>Modify</a></td>`; 
+    dataTable += `<td><a href='/inv/delete/${element.inv_id}' title='Click to delete'>Delete</a></td></tr>`; 
+    }) 
+    dataTable += '</tbody>'; 
+    // Display the contents in the Inventory Management view 
+    inventoryDisplay.innerHTML = dataTable; 
+}
