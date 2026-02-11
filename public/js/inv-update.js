d58d57a5a4c7664e93fa08dc51402f5b792b123f
nanurodriguez <nancy95rod@gmail.com>
2026-02-10 22:33:50 -0700

    adding message to the update button

--- public/js/inv-update.js
+++ public/js/inv-update.js
@@ -1,5 +1,9 @@
+// JavaScript to enable the Update button when a change is made to the form/ otherwise disable the button and show a message to the user that they need to make a change to enable the button
 const form = document.querySelector("#updateForm")
+const updateBtn = document.querySelector("button")
+const message = document.querySelector("#updateMessage")
+updateBtn.setAttribute("disabled", true)
     form.addEventListener("change", function () {
-        const updateBtn = document.querySelector("button")
         updateBtn.removeAttribute("disabled")
+        message.style.display = "none"
     })
